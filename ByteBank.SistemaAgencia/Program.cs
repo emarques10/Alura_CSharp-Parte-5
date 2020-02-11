20572c2f53ba99ceb7e0ad786576208ffde745a1
emarqu <emarques@processwaregroup.com>
2020-02-11 11:45:37 -0300

    Modificadores de acesso em bibliotecas

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,18 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(111,1234);
+            
+        }
+    }
+}
